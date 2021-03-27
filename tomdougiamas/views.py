7b175bd5fff53ce51c72e9d6ba502fda445d0fd0
exciteabletom <tom@digitalnook.net>
2021-03-28 01:54:17 +0800

    WIP

--- tomdougiamas/views.py
+++ tomdougiamas/views.py
@@ -3,6 +3,19 @@ from django.shortcuts import render
 
 app_name = "tomdougiamas"
 
+
 # Create your views here.
 def index(request):
     return render(request, "tomdougiamas/index.html")
+
+
+def about(request):
+    return render(request, "tomdougiamas/about.html")
+
+
+def projects(request):
+    return render(request, "tomdougiamas/projects.html")
+
+
+def links(request):
+    return render(request, "tomdougiamas/links.html")
