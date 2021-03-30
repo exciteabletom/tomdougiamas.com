6cd0efedfc3cc97b707931dce5fdd864223412b7
exciteabletom <tom@digitalnook.net>
2021-03-30 09:24:17 +0800

    Add rm to update.sh

--- update.sh
+++ update.sh
@@ -5,4 +5,7 @@ command cd "$(dirname "$0")" || exit 1
 
 git pull || exit 1
 python3 manage.py makemigrations
+
+# Django doesn't register if a file is deleted from project static folders
+command rm -rf ./static/
 python3 manage.py collectstatic <<< yes
