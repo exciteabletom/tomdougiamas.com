e73eb2fb8ffdb28c5f25f86c5b54c405c802332f
exciteabletom <tom@digitalnook.net>
2021-04-01 11:18:25 +0800

    add compression of css; add requirements.txt

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -16,106 +16,104 @@ from pathlib import Path
 PRODUCTION_ENABLED = bool(os.environ.get("PERSONAL_SITE_PRODUCTION_MODE"))
 
 if PRODUCTION_ENABLED:
-    with open('/etc/secret_key.txt') as f:
-        SECRET_KEY = f.read().strip()
+	with open('/etc/secret_key.txt') as f:
+		SECRET_KEY = f.read().strip()
 
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
+	CSRF_COOKIE_SECURE = True
+	SESSION_COOKIE_SECURE = True
 
-    DEBUG = False
+	COMPRESS_OFFLINE = True
+
+	DEBUG = False
 
 # If development environment
 else:
-    # Throwaway key for development only!
-    SECRET_KEY = '^vrua7@34=dh%q846*=97e(1f%^v(9#0u^%m84x$6+vauln8vj'
+	# Throwaway key for development only!
+	SECRET_KEY = '^vrua7@34=dh%q846*=97e(1f%^v(9#0u^%m84x$6+vauln8vj'
 
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
+	# SECURITY WARNING: don't run with debug turned on in production!
+	DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = [
-    "tomdougiamas.com",
-    "localhost",
-    "127.0.0.1",
+	"tomdougiamas.com",
+	"localhost",
+	"127.0.0.1",
 ]
 
-
-# Application definition
-
 INSTALLED_APPS = [
-    'tomdougiamas.apps.TomdougiamasConfig',
+	'tomdougiamas.apps.TomdougiamasConfig',
 
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'compressor',
+
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'personal_site.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates',],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [
+			BASE_DIR / 'templates', ],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'personal_site.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'db.sqlite3',
+	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -130,9 +128,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
+
+STATICFILES_FINDERS = {
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	'compressor.finders.CompressorFinder',
+}
+
+COMPRESS_ENABLED = True
