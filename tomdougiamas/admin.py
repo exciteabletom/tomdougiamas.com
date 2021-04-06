48abe3ff7eeddfefc4f2868610855813800d5341
exciteabletom <tom@digitalnook.net>
2021-04-06 16:05:55 +0800

    start work on blog

--- tomdougiamas/admin.py
+++ tomdougiamas/admin.py
@@ -1,1 +1,7 @@
+from django.contrib import admin
+
+from .models import BlogPost, BlogComment
+
 # Register your models here.
+
+admin.register(BlogPost)
--- tomdougiamas/models.py
+++ tomdougiamas/models.py
@@ -1,1 +1,17 @@
+from django.db import models
+
+
 # Create your models here.
+
+
+class BlogPost(models.Model):
+    blog_title = models.TextField()
+    blog_text = models.TextField()
+    pub_date = models.DateTimeField("Date published")
+
+
+class BlogComment(models.Model):
+    blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    pub_date = models.DateTimeField("Date published", null=True)
+    votes = models.IntegerField(default=0)
