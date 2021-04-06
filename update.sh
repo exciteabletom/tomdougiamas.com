7f80e967b2da4d13b493957b817e4e0c0657fa00
exciteabletom <tom@digitalnook.net>
2021-04-06 15:43:02 +0800

    add requirements to update.sh

--- update.sh
+++ update.sh
@@ -7,10 +7,13 @@ command cd "$(dirname "$0")" || exit 1
 export PERSONAL_SITE_PRODUCTION_MODE=yes
 
 git pull || exit 1
+
+python3 -m pip install -r requirements.txt
+
 python3 manage.py migrate 
 
-# Django doesn't register if a file is deleted from project static folders
 command rm -rf static
+
 python3 manage.py collectstatic <<< yes
 python3 manage.py compress 
 
