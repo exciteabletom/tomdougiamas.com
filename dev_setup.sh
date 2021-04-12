6e6833f4fa3cd1e9298e7b2b349b6c3ede769fc5
exciteabletom <tom@digitalnook.net>
2021-04-12 22:05:37 +0800

    pre-commit fixes; dev setup change

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,7 +3,7 @@
 # Go to project root
 command cd "$(dirname "$0")" || exit 1
 
-if ! find . | grep ./.venv/; then
+if ! find . | grep --quiet ./.venv/; then
   sleep 100
   python3 -m venv .venv
 fi
