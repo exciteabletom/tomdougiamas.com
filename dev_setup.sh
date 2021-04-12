6a6ec237f5d339fda787f28ac4979e0b8a05dc37
exciteabletom <tom@digitalnook.net>
2021-04-12 22:03:48 +0800

    add dev setup script

---
+++ dev_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# Go to project root
+command cd "$(dirname "$0")" || exit 1
+
+if ! find . | grep ./.venv/; then
+  sleep 100
+  python3 -m venv .venv
+fi
+
+python3 -m pip install --upgrade pip wheel
+python3 -m pip install -r requirements.txt -r requirements-dev.txt
+
+pre-commit install
+pre-commit run
