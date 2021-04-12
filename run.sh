483af93bb57214e66c4afb43591bdd04912d5d91
exciteabletom <tom@digitalnook.net>
2021-04-12 22:28:37 +0800

    fix up scripts

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # Go to project root
 command cd "$(dirname "$0")" || exit 1
 
-source .venv/bin/activate
+. .venv/bin/activate
 
 export PERSONAL_SITE_PRODUCTION_MODE=yes
 gunicorn --error-logfile errors.txt --workers 2 --bind unix:tomdougiamas.com.sock -m 007 personal_site.wsgi
