dd15955ebaa0442732e1dbc4daf1b5e8d7cb2815
exciteabletom <tom@digitalnook.net>
2021-04-12 22:28:37 +0800

    fix up scripts

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,10 +3,11 @@
 # Go to project root
 command cd "$(dirname "$0")" || exit 1
 
-if ! find . | grep --quiet ./.venv/; then
-  sleep 100
+if ! find . | grep --quiet .venv/bin/activate; then
   python3 -m venv .venv
 fi
+
+. .venv/bin/activate
 
 python3 -m pip install --upgrade pip wheel
 python3 -m pip install -r requirements.txt -r requirements-dev.txt
