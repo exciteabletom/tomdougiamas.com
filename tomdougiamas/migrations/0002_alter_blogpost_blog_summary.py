6fb314517a6a524278ce1de707d7622515da758b
exciteabletom <tom@digitalnook.net>
2021-05-07 14:56:37 +0800

    reformat

--- tomdougiamas/migrations/0002_alter_blogpost_blog_summary.py
+++ tomdougiamas/migrations/0002_alter_blogpost_blog_summary.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("tomdougiamas", "0001_squashed_0007_auto_20210427_1436"),
     ]
