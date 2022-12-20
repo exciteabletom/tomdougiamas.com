e61b780c12913b87cd8b0d40023cf003483de60d
exciteabletom <tom@digitalnook.net>
2022-12-21 01:32:05 +0800

    make projects look better

--- tomdougiamas/views.py
+++ tomdougiamas/views.py
@@ -7,4 +7,8 @@ app_name = "tomdougiamas"
 
 def index(request):
     project_items = reversed(Project.objects.order_by("project_date"))
-    return render(request, "tomdougiamas/index.html", context={"projects": project_items, "links": Link.objects.all()})
+    return render(
+        request,
+        "tomdougiamas/index.html",
+        context={"projects": project_items, "links": Link.objects.all()},
+    )
